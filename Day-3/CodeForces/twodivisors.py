d221584d67d86abe3e9470687d22d9b14f9085e9
MridulPathania01 <mridulpathania05@gmail.com>
2025-06-20 11:04:02 +0530

    forth commit

--- Day-3/CodeForces/twodivisors.py
+++ Day-3/CodeForces/twodivisors.py
@@ -1,7 +1,15 @@
-def find_two_divisors(a, b):
-    for x in range(a, b + 1):
-        if x % 2 == 0 and x > 2:
-            return (2, x - 2)
-    return -1
-a, b = map(int, input().split())
-print(find_two_divisors(a, b))
+import math
+
+t = int(input())
+for _ in range(t):
+    a, b = map(int, input().split())
+    n = a + b
+    divisors = set()
+    for i in range(1, int(math.isqrt(n)) + 1):
+        if n % i == 0:
+            if i < n:
+                divisors.add(i)
+            if n // i < n:
+                divisors.add(n // i)
+    largest = sorted(divisors)[-2:]
+    print(largest[0] * largest[1])
--- Day-3/CodeForces/wonderfulsticks.py
+++ Day-3/CodeForces/wonderfulsticks.py
@@ -1,2 +1,18 @@
-#wonderfulsticks solution
-    
+def test():
+    n = int(input())
+    s = input().strip()
+    l = 1
+    r = n
+    a = [0] * n
+    for i in range(n - 2, -1, -1):
+        if s[i] == '<':
+            a[i + 1] = l
+            l += 1
+        if s[i] == '>':
+            a[i + 1] = r
+            r -= 1
+    a[0] = l
+    print(' '.join(map(str, a)))
+t = int(input())
+for _ in range(t):
+    test()
